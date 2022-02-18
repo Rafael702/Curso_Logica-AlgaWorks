e1e27277c2efa5199664794040407dd41faa8ec0
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-02-18 14:51:38 -0300

    Concluir exercicio 2.

---
+++ Modulo 7/src/exercicio2/FaturamentoSemanal.java
@@ -0,0 +1,36 @@
+package exercicio2;
+
+public class FaturamentoSemanal {
+	
+	public static void main(String[] args) {
+		Double[] semanaUm = new Double[]{ 1000.0, 1500.0, 1250.0, 2100.0, 1350.0, 970.0, 0.0 };
+		Double[] semanaDois = new Double[]{ 1050.0, 2500.0, 3250.0, 1100.0, 1430.0, 900.0, 0.0 };
+		Double[] semanaTres = new Double[]{ 1700.0, 1300.0, 1450.0, 2000.0, 1390.0, 800.0, 0.0 };
+		Double[] semanaQuatro = new Double[]{ 2100.0, 1600.0, 20000.0000, 2100.0, 1220.0, 998.0, 0.0 };
+
+		Double[][] mes = new Double[][] { semanaUm, semanaDois, semanaTres, semanaQuatro };
+		
+		Double maiorFaturamento = 0.0;
+		Integer semanaComMaiorFaturamento = 0;
+		Integer diaDaSemana = 0;
+				
+		for(int contador = 0; contador < mes.length; contador++) {
+						
+			Double[] semana = mes[contador]; 
+			
+			for(int vetorDeSemana = 0; vetorDeSemana < semana.length; vetorDeSemana++) {
+				if(semana[vetorDeSemana] >= maiorFaturamento) {
+					semanaComMaiorFaturamento = contador + 1;
+					maiorFaturamento = semana[vetorDeSemana];
+					diaDaSemana = vetorDeSemana +1; 
+				}
+			}
+		}
+		
+		
+		System.out.println("Maior Faturamento: R$" + maiorFaturamento + ", no " + diaDaSemana
+				+ "° dia da " + semanaComMaiorFaturamento + "° Semana do mês");
+		
+	}
+
+}
