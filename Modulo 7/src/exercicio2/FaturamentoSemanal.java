a39ba731b29fa4723ad587d342e0ed0cd0a4839d
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-02-18 15:20:07 -0300

    Incluir resolucao da alga works.

--- Modulo 7/src/exercicio2/FaturamentoSemanal.java
+++ Modulo 7/src/exercicio2/FaturamentoSemanal.java
@@ -6,10 +6,10 @@ public class FaturamentoSemanal {
 		Double[] semanaUm = new Double[]{ 1000.0, 1500.0, 1250.0, 2100.0, 1350.0, 970.0, 0.0 };
 		Double[] semanaDois = new Double[]{ 1050.0, 2500.0, 3250.0, 1100.0, 1430.0, 900.0, 0.0 };
 		Double[] semanaTres = new Double[]{ 1700.0, 1300.0, 1450.0, 2000.0, 1390.0, 800.0, 0.0 };
-		Double[] semanaQuatro = new Double[]{ 2100.0, 1600.0, 20000.0000, 2100.0, 1220.0, 998.0, 0.0 };
+		Double[] semanaQuatro = new Double[]{ 2100.0, 1600.0, 1850.0, 2100.0, 1220.0, 998.0, 0.0 };
 
 		Double[][] mes = new Double[][] { semanaUm, semanaDois, semanaTres, semanaQuatro };
-		
+		/*
 		Double maiorFaturamento = 0.0;
 		Integer semanaComMaiorFaturamento = 0;
 		Integer diaDaSemana = 0;
@@ -30,7 +30,31 @@ public class FaturamentoSemanal {
 		
 		System.out.println("Maior Faturamento: R$" + maiorFaturamento + ", no " + diaDaSemana
 				+ "° dia da " + semanaComMaiorFaturamento + "° Semana do mês");
+		*/
 		
+		//Outra Forma de Resolver
+		
+		Double maiorFaturamento = 0.0;
+		Integer semanaGanhadora = null; 
+		
+		for (int i = 0; i < mes.length; i++) {
+			Double[] semana = mes[i];
+			
+			Double faturamentoSemana = 0.0;
+			
+			for (int y = 0; y < semana.length; y++) {
+				faturamentoSemana += semana[y];
+			}
+			
+			Boolean esseFaturamentoEOMaior = faturamentoSemana > maiorFaturamento;
+			
+			if (esseFaturamentoEOMaior) {
+				maiorFaturamento = faturamentoSemana;
+				semanaGanhadora = i + 1; 
+			}
+		}
+
+		System.out.println("A semana " + semanaGanhadora + " teve o maior faturamento que foi de " + maiorFaturamento + ".");
 	}
 
 }
