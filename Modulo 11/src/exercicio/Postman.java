e5ea3b1f1bf6ec947d3628237918ccfbec07a0c2
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-03-02 16:06:02 -0300

    Refatorar codigo.

--- Modulo 11/src/exercicio/Postman.java
+++ Modulo 11/src/exercicio/Postman.java
@@ -6,20 +6,28 @@ import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.SimpleEmail;
 
 public class Postman {
-	static void enviar(String para,String assunto, String mensagem) throws Exception {
+	static void enviar(String mensagem) throws Exception {
 		Email email = new SimpleEmail();
 		email.setHostName("smtp.gmail.com");
 		email.setSmtpPort(465);
 		email.setAuthenticator(new DefaultAuthenticator("rafaeldealmeida.192001", "3tncHyqbHS"));
 		email.setSSLOnConnect(true);
 		email.setFrom("rafaeldealmeida.192001@gmail.com");
-		email.setSubject(assunto);
+		email.setSubject(informarAssunto());
 		email.setMsg(mensagem);
-		email.addTo(para);
+		email.addTo(informarEmailDestino());
 		carregarEnvioDeMensagem();
 		email.send();
 		
 		
+	}
+	
+	static String informarEmailDestino() {
+		return EnvioDeTarefas.scanner("Informe o email de destino:").next();
+	}
+	
+	static String informarAssunto() {
+		return EnvioDeTarefas.scanner("Informe o assunto de destino:").next();
 	}
 	
 	static void carregarEnvioDeMensagem() throws Exception{
